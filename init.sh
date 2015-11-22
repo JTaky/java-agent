5edf7db113eccee6f666121bea77ee0685383540
Alex Nitavsky <Alex.Nitavsky@grammarly.com>
2015-11-22 18:43:32 +0100

    add script to download pre-commit hook

---
+++ init.sh
@@ -0,0 +1,5 @@
+curl -o .git/hooks/pre-commit "https://raw.githubusercontent.com/JTaky/githooks/master/ci/pre-commit"
+echo "pre-commit content:"
+echo "----------------------------------"
+cat .git/hooks/pre-commit
+echo "----------------------------------"
